9c342e8748b808235cf4e1ab9e6b1e49068dcf03
Marrio <marrio.andrei@gmail.com>
2024-04-28 14:41:40 +0200

    Start

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Battlefield_NTI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
